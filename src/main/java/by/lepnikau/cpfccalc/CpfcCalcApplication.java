c2c6e763b174d9984e856cc120b41577edcf8ba9
ViachaslauL <Lotussutol@tut.by>
2022-05-29 18:45:13 +0300

    start project

---
+++ src/main/java/by/lepnikau/cpfccalc/CpfcCalcApplication.java
@@ -0,0 +1,13 @@
+package by.lepnikau.cpfccalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpfcCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpfcCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/lepnikau/cpfccalc/CpfcCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lepnikau.cpfccalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpfcCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
